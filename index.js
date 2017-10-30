58e59e50f9eebefa78b9fc7f3c3d50cbc98a6cb3
Enieber Cunha <eniebercunha@gmail.com>
2017-10-30 01:25:57 -0300

    feat(enieber) add packages

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const restify = require('restify');
+const jwt = require('jsonwebtoken');
+const createDb = require('./createDb');
 
 const server = require('./src/server');
 const routers = require('./routes');
 
-server
- .listen(
+server.listen(
    3000,
    () => console.log('%s listening at %s',
    server.name, server.url)
